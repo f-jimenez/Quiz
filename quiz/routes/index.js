714c15c54a956abff750e7ea1975eb4943a48eeb
fjimenez <f.jimenez@runavisual.com>
2015-06-06 19:03:15 +0000

    Primera pregunta

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+
+
+/* GET question */
+router.get('/quizes/question', quizController.question);
+
+
+/* GET answer */
+router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
