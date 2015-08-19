f403b5f316dc13d588136d5f16dbbb64e8d17f53
fjimenez <f.jimenez@runavisual.com>
2015-08-19 14:36:12 +0000

    Moderación de comentarios

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res, next) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 
 
 // Definicion de rutas de sesion
@@ -46,6 +47,7 @@ router.get('/author', quizController.author);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 
 module.exports = router;
